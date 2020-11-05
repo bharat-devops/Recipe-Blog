fe11000595b1e9226c0e9bc554f8bc655616d1a6
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-05 20:34:26 +0530

    added templates for blog and index

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 from marketing.models import Signup
@@ -7,7 +8,7 @@ def index(request):
     latest = Post.objects.order_by('-timestamp')[0:3]
 
     if request.method == 'POST':
-        email = request.Post["email"]
+        email = request.POST["email"]
         new_signup = Signup()
         new_signup.email = email
         new_signup.save()
@@ -21,7 +22,22 @@ def index(request):
 
 
 def blog(request):
-    return render(request, 'blog.html', {})
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list,4)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_querset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_querset = paginator.page(1)
+    except EmptyPage:
+        paginated_querset = paginator.page(paginator.num_pages)
+
+    context = {
+        'page_request_var': page_request_var,
+        'queryset': paginated_querset
+    }
+    return render(request, 'blog.html', context)
 
 def post(request):
     return render(request, 'post.html', {})
