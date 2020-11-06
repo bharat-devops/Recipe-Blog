bc693bf6843c20fd2545466a3d0812013fbe4942
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-06 20:09:32 +0530

    dotenv changes

--- blog/settings.py
+++ blog/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 ## CUSTOM Setting for .env file 
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+    dotenv.find_dotenv(dotenv_file)
 
 
 
