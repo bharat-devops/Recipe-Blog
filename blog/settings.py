2df30a8ef5deb01dc97817f5d973808eb81046ee
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-06 22:43:20 +0530

    dotenv changes

--- blog/settings.py
+++ blog/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 ## This database configuration uses sqlite on local and postgresql on Heroku. It uses .env and dj_database_url settings
 
