d0ae69e59f57ad512fe56a75ade8c78467001df6
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-08 16:04:57 +0530

    added s3 bucket details

--- blog/settings.py
+++ blog/settings.py
@@ -134,10 +134,15 @@ AWS_STORAGE_BUCKET_NAME = 'feastbeast-bucket'
 AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
+AWS_LOCATION = 'feastbeast/static_in_env'
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
 # you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_in_env'),
+]
+
