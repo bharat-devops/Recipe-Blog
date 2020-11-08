249d13f9bc74753c4fadb245c7ca1de604006fe2
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-08 10:45:50 +0530

    added s3 bucket details

--- blog/settings.py
+++ blog/settings.py
@@ -145,7 +145,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'feastbeast'
+AWS_LOCATION = 'feastbeast/static_in_env'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_in_env'),
