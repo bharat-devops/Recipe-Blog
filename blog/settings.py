0bb068dcf1a9d24e81217a814935821dc0a75722
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-08 22:16:11 +0530

    added s3 bucket details

--- blog/settings.py
+++ blog/settings.py
@@ -3,7 +3,7 @@ import os
 import django_heroku
 import dj_database_url
 import dotenv
-import s3utils
+import blog.s3utils
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
