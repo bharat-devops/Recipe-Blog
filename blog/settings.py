2742ed5f0045ec6922e03ce1923c29039c9ccdec
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-08 07:23:29 +0530

    added s3 bucket details

--- blog/settings.py
+++ blog/settings.py
@@ -135,7 +135,5 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
-AWS_S3_REGION_NAME = "ap-south-1"
 
-AWS_S3_SIGNATURE_VERSION = "s3v4"
 
