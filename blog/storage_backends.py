08e94d369b1188a6db66457d59c62d8ee337388f
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-09 08:43:42 +0530

    added s3 bucket details

--- blog/storage_backends.py
+++ blog/storage_backends.py
@@ -3,11 +3,11 @@ from django.conf import settings
 
 
 class StaticStorage(S3Boto3Storage):
-    location = 'static'
+    location = 'feastbeast/static'
     default_acl = 'public-read'
 
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = 'media'
+    location = 'feastbeast/media'
     default_acl = 'public-read'
     file_overwrite = False
