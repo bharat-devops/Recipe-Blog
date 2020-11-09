63d995aed9ac5165301c892c191a90ee210a54df
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-09 08:39:39 +0530

    added s3 bucket details

--- blog/s3utils.py
+++
@@ -1,7 +0,0 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-def StaticRootS3Boto3Storage(): return S3Boto3Storage(location='static_root')
-
-
-def MediaRootS3Boto3Storage(): return S3Boto3Storage(location='media_root')
--- blog/settings.py
+++ blog/settings.py
@@ -3,7 +3,7 @@ import os
 import django_heroku
 import dj_database_url
 import dotenv
-import blog.s3utils
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,24 +132,50 @@ USE_TZ = True
 
 #### CONFIGURATION on S3 BUCKET ###
 
-AWS_STORAGE_BUCKET_NAME = 'feastbeast-bucket'
-AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_LOCATION = 'feastbeast/static_in_env'
-# Tell django-storages the domain to use to refer to static files.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_STORAGE_BUCKET_NAME = 'feastbeast-bucket'
+# AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# AWS_LOCATION = 'feastbeast/static_in_env'
+# # Tell django-storages the domain to use to refer to static files.
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
-# you run `collectstatic`).
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_in_env'),
-]
+# # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# # you run `collectstatic`).
+# #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static_in_env'),
+# ]
 
 #STATIC_ROOT = os.path.join(AWS_LOCATION, 'static_root')
 
-DEFAULT_FILE_STORAGE = blog.s3utils.MediaRootS3Boto3Storage
-STATICFILES_STORAGE = blog.s3utils.StaticRootS3Boto3Storage
+
+### NEW Solution
+USE_S3 = os.getenv('USE_S3')
+
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'blog.storage_backends.StaticStorage'
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'blog.storage_backends.PublicMediaStorage'
+else:
+    STATIC_URL = '/staticfiles/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_URL = '/mediafiles/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
