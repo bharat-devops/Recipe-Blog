3c70a579b5d65442f8acafef54f3ca25b31cac32
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-09 09:25:41 +0530

    added s3 bucket details

--- blog/settings.py
+++ blog/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '*3se1=g@!@e^dmpm8171p@h2n+s4i&xq+s4&9c&29d6v)unm=u'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+## SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['recipe4everyone.herokuapp.com','127.0.0.1']
