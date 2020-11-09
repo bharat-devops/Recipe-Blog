7892eea6e2f9366e50313c28374885f6fa8f81d5
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-09 10:22:58 +0530

    added s3 bucket details

--- blog/settings.py
+++ blog/settings.py
@@ -161,6 +161,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'feastbeast-bucket'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = 'public-read'
 # s3 static settings
 STATIC_LOCATION = 'static'
 STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, STATIC_LOCATION)
