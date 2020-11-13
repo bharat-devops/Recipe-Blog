5c166d8d1d1c0c2c64b35e89d5734918d50cce25
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-13 17:37:17 +0530

    migration issue

--- posts/models.py
+++ posts/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     overview = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    content = HTMLField()
+    #content = HTMLField()
     comment_count = models.IntegerField(default=0)
     view_count = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
