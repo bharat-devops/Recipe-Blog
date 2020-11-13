70efc5fbea094ce15d78aab4dc845230bab79ac9
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-13 18:01:07 +0530

    migration issue

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 # TinyMCE
-from tinymce.models import HTMLField
+from tinymce import models as tinymce_models
 # Create your models here.
 
 User = get_user_model()
@@ -25,7 +25,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     overview = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    #content = HTMLField()
+    content = tinymce_models.HTMLField()
     comment_count = models.IntegerField(default=0)
     view_count = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
