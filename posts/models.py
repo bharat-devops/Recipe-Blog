1b8f7572e6edf9a7a6f40f048b1dfd1d4de97aa3
unknown <k_bharath_hegde@yahoo.co.in>
2020-11-13 14:51:40 +0530

    tinymce added as widget using form and model

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+# TinyMCE
+from tinymce.models import HTMLField
 # Create your models here.
 
 User = get_user_model()
@@ -23,6 +25,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     overview = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    content = HTMLField()
     comment_count = models.IntegerField(default=0)
     view_count = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
