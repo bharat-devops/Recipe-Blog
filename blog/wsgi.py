90fe0276039f204e0197d2156422fe61174f6e90
unknown <k_bharath_hegde@yahoo.co.in>
2020-12-02 17:02:14 +0530

    commited for heroku settings

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -2,7 +2,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings_dev')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings_dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
 application = get_wsgi_application()
 
